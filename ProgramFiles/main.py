a583dea1646bb644b89c75baf40158370fd23e29
MaciejSalamonski <macieksalamonski@gmail.com>
2021-05-23 02:04:25 +0200

    main - added

--- ProgramFiles/main.py
+++ ProgramFiles/main.py
@@ -1,8 +1,51 @@
 import sys
 import os
 sys.path.append(os.path.abspath("Functions"))
-from KeysGenerator import *
+
+from CipherBlockChaining import CipherBlockChaining
+from ElectronicCodeBook import ElectronicCodeBook
+from ElectronicCodeBookBasedOnLibraries import ElectronicCodeBookBasedOnLibraries
+from KeysGenerator import GetKeys
 
 if __name__ == '__main__':
 
-    CreateKeyFiles(1024)
+    keyLength = 1024
+    n, e, d = GetKeys(keyLength)
+
+    imagePath = '../Images/example.png'
+
+    encryptedElectronicCodeBookPath = '../EncryptedImages/exampleEncryptedElectronicCodeBook.png'
+    decryptedElectronicCodeBookPath = '../DecryptedImages/exampleDecryptedElectronicCodeBook.png'
+
+    encryptedElectronicCodeBookBasedOnLibrariesPath = '../EncryptedImages/exampleEncryptedElectronicCodeBookBasedOnLibraries.png'
+    decryptedElectronicCodeBookBasedOnLibrariesPath = '../DecryptedImages/exampleDecryptedElectronicCodeBookBasedOnLibraries.png'
+
+    encryptedCipherBlockChainingPath = '../EncryptedImages/exampleEncryptedCipherBlockChaining.png'
+    decryptedCipherBlockChainingPath = '../DecryptedImages/exampleDecryptedCipherBlockChaining.png'
+
+    electronicCodeBook = ElectronicCodeBook(imagePath, \
+                                            encryptedElectronicCodeBookPath, \
+                                            decryptedElectronicCodeBookPath, \
+                                            n, \
+                                            e, \
+                                            d)
+    electronicCodeBook.PngEncryption()
+    electronicCodeBook.PngDecryption()
+
+    electronicCodeBookBasedOnLibraries = ElectronicCodeBookBasedOnLibraries(imagePath, \
+                                                                            encryptedElectronicCodeBookBasedOnLibrariesPath, \
+                                                                            decryptedElectronicCodeBookBasedOnLibrariesPath, \
+                                                                            n, \
+                                                                            e, \
+                                                                            d)
+    electronicCodeBookBasedOnLibraries.PngEncryption()
+    electronicCodeBookBasedOnLibraries.PngDecryption()
+
+    cipherBlockChaining = CipherBlockChaining(imagePath, \
+                                              encryptedCipherBlockChainingPath, \
+                                              decryptedCipherBlockChainingPath, \
+                                              n, \
+                                              e, \
+                                              d)
+    cipherBlockChaining.PngEncryption()
+    cipherBlockChaining.PngDecryption()
